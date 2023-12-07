15ba84b5b66dbb511a6513c459ca5255ebfeb871
Yaroslav Mikhaylov <sponaproz@gmail.com>
2023-12-07 19:35:09 +0300

    setup project, add config

--- cmd/internal/config/config.go
+++ cmd/internal/config/config.go
@@ -1,8 +1,50 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
-	env: "local" // local, dev, prod
-	storage_path: "./storage/storage.db"
-	http_server:
-		address: "localhost:8082"
-		timeout: 4s # read query & response timeout
-		idle_timeout: 60s # client-server connection lifespan
+	Env         string `yaml:"env" env-default:"prod"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8082"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	// If CONFIG_PATH is not set, use .env file
+	if configPath == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+		configPath = os.Getenv("CONFIG_PATH")
+
+		if configPath == "" {
+			log.Fatalf("CONFIG_PATH is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("CONFIG_PATH is not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Cannot read config: %s", err)
+	}
+
+	return &cfg
 }
--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,4 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"url-shortener/cmd/internal/config"
+)
+
 func main() {
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+
 	// TODO: init config: cleanenv
 	// TODO: init logger
 	// TODO: init storage: SQLite
