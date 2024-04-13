638dc87d760f91e5098574bac2e00d1ad294c834
Yaroslav Mikhaylov <sponaproz@gmail.com>
2024-04-13 10:02:08 +0300

    add logger to main

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
-	"fmt"
-
+	"log/slog"
+	"os"
 	"url-shortener/cmd/internal/config"
 
 	"golang.org/x/exp/slog"
 )
 
+const (
+	EnvLocal = "local"
+	EnvProd  = "prod"
+	EnvDev   = "dev"
+)
+
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
-func setupLogger(env string) {
-	var log *slog.Logger
-}
-	// TODO: init logger
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug message are enabled")
 	// TODO: init storage: SQLite
 	// TODO: init router
 	// TODO: start server
+}
+
+func setupLogger(env string) {
+	var log *slog.Logger
+
+	switch env {
+	case EnvLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case EnvDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case EnvProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
